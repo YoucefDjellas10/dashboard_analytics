e1385a6bfa5589f7fe065fc913e20eb20cb7d3b5
samahdjellas <samahdjellas10@gmail.com>
2026-06-08 15:18:12 +0200

    add models files

--- static/src/js/ca_dashboard.js
+++ static/src/js/ca_dashboard.js
@@ -24,8 +24,6 @@ export class CaDashboard extends Component {
             rows          : [],
             annee_n       : currentYear,
             annee_n1      : currentYear - 1,
-            pie_data      : [],
-            pie_data_n1   : [],
             years         : years,
         });
 
@@ -129,13 +127,10 @@ export class CaDashboard extends Component {
             this.state.rows = rows;
 
         } finally {
-            await Promise.all([this._loadPieData(), this._loadPieDataN1()]);
             this.state.loading = false;
             setTimeout(() => {
                 this._renderChart();
                 this._renderChartLine();
-                this._renderChartPie();
-                this._renderChartPieN1();
             }, 50);
         }
     }
