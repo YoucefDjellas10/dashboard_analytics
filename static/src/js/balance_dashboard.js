9c7aebb17836c0aa66bb5713a5ee61838948be53
samahdjellas <samahdjellas10@gmail.com>
2026-06-08 15:30:26 +0200

    add models files

--- static/src/js/balance_dashboard.js
+++ static/src/js/balance_dashboard.js
@@ -21,8 +21,6 @@ export class BalanceDashboard extends Component {
             rows          : [],
             annee_n       : currentYear,
             annee_n1      : currentYear - 1,
-            pie_data      : [],
-            pie_data_n1   : [],
             years         : years,
         });
 
@@ -279,13 +277,11 @@ export class BalanceDashboard extends Component {
             this.state.rows = rows;
 
         } finally {
-            await Promise.all([this._loadPieData(), this._loadPieDataN1()]);
             this.state.loading = false;
             setTimeout(() => {
                 this._renderChart();
                 this._renderChartLine();
-                this._renderChartPie();
-                this._renderChartPieN1();
+
             }, 50);
         }
     }
